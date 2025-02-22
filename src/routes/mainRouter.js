cb3f3324291cb8e2b38ba70d600a2e1aad840217
Onu Daniel <106540842+ECASHparttern@users.noreply.github.com>
2025-02-22 22:41:37 +0100

    Update mainRouter.js
    connected the report router to the main router

--- src/routes/mainRouter.js
+++ src/routes/mainRouter.js
@@ -1,8 +1,10 @@
 const userRouter = require('./userRoutes');
+const reportRoutes = require('./reportRoutes')
 const express = require('express');
 
 const router = express.Router();
 
 router.use('/users', userRouter);
+router.use('/report', reportRoutes);
 
 module.exports = router;
